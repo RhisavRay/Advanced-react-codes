60c96b4881819b70510f5d2ecb021b6c9c03b5b2
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-14 10:52:44 +0530

    Transformation of list in JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import './App.css';
+const dessert_list = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "The best Tiramisu in town",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$5.00"
+  },
+  {
+    id: "1",
+    title: "Lemon Ice Cream",
+    description: "Mind blowing taste!",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$4.50"
+  },
+  {
+    id: "1",
+    title: "Chocolate Mouse",
+    description: "Unexplored flavour",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$6.00"
+  }
+];
 
-function App() {
-  return 
+const top_desserts = dessert_list.map(dessert => 
+  {
+    return {
+      content: `${dessert.title} - ${dessert.description}`,
+      price: dessert.price
+    }
+  })
+
+function App()
+{
+  console.log(top_desserts)
+
+  return <h1>Explore the console output</h1>
 }
 
 export default App;
