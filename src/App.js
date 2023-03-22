67cad13f718fe04754e3373ae953f8c19e178ce3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-22 15:56:26 +0530

    Feedback form with React

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import { useState } from "react";
 
 function App()
 {
-  const [name, setName] = useState('')
+  const [score, setScore] = useState("10")
+  const [comment, setComment] = useState("")
 
   const handleSubmit = (e) =>
   {
     e.preventDefault()
-    console.log(`Form was submtted successfully! Welcome ${name}`)
-    setName('')
+    console.log(`Thank you for your feedback. You have given us a score of ${score}⭐!`)
+    setScore("10")
+    setComment("")
   }
 
   return(
     <div className="App">
       <form onSubmit={handleSubmit}>
         <fieldset>
-          <legend>Forms with React.js</legend>
+          <legend>Feedback Form form Little Lemon</legend>
           <div className="Field">
-            <label htmlFor="name">Name: </label>
-            <input id="name" type='text' placeholder="Name..." name="name" value={name} onChange={e => setName(e.target.value)}/>
+            <label>Rate the food</label>
+            <br></br>
+            <input type='range' min='1' max='10' value={score} onChange={e => setScore(e.target.value)}/>
+            <label>{score}⭐</label>
           </div>
-          <button disabled={!name} type="submit">Submit</button>
+          <div className="Field">
+            <label htmlFor="comment">Additional comments</label>
+            <br></br>
+            <textarea id="comment" placeholder="Comments please..." value={comment} onChange={e => setComment(e.target.value)}/>
+          </div>
+          <button disabled={!comment} type="submit">Submit</button>
         </fieldset>
       </form>
     </div>
