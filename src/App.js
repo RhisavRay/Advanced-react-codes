ebbd0e8241bd691f9f2798a5d8773acdaa5af29a
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-28 08:07:24 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,63 +1,8 @@
 import "./App.css";
-import { useState } from "react";
-
-function GoalForm(props)
-{
-  const [formData, setFormData] = useState({goal: "", by: ""})
-
-  function changeHandler(e)
-  {
-    setFormData({...formData, [e.target.name]: e.target.value})
-  }
-
-  function submitHandler(e)
-  {
-    e.preventDefault()
-    props.onAdd(formData)
-    setFormData({goal: "", by: ""})
-  }
-
-  return(
-    <>
-      <h1>My Little Lemon Goals</h1>
-      <form onSubmit={submitHandler}>
-        <input type="text" name="goal" placeholder="Goal" value={formData.goal} onChange={changeHandler}/>
-        <input type="text" name="by" placeholder="By..." value={formData.by} onChange={changeHandler}/>
-        <button type="submit">Submit Goal</button>
-      </form>
-    </>
-  )
-}
-
-function ListOfGoals(props)
-{
-  return(
-    <ul>
-      {props.allGoals.map((g) =>
-      {
-        <li key={g.goal}>
-          <p>My goal is to {g.goal}, by {g.by}</p>
-        </li>
-      })}
-    </ul>
-  )
-}
 
 function App()
 {
-  const [allGoals, setAllGoals] = useState([])
-
-  function addGoal(goal)
-  {
-    setAllGoals([...allGoals, goal])
-  }
-  
-  return(
-    <div>
-      <GoalForm onAdd={addGoal}/>
-      <ListOfGoals allGoals={allGoals}/>
-    </div>
-  )
+  return
 }
 
 export default App
