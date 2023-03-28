d28d7de5878cb350b56c3a5287e95716f8f80218
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-28 08:27:10 +0530

    Lab: Managing state within a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 
 function App()
 {
-  return
+  const [giftCard, setGiftCard] = useState(
+    {
+      firstName: "Jennifer",
+      lastName: "Smith",
+      text: "Free dinner for 4 guests",
+      valid: true,
+      instructions: "To use your coupon, click the button below."
+    }
+  )
+
+  function spendGiftCard()
+  {
+    setGiftCard({...giftCard,
+      text: "Your coupon has been used.",
+      instructions: "Please visit our restaurant to renew your gift card",
+      valid: false})
+  }
+
+  return(
+    <div style={{padding: '40px'}}>
+      <h1>Gift Card Page</h1>
+      <h2>Customer: {giftCard.firstName} {giftCard.lastName}</h2>
+      <h3>{giftCard.text}</h3>
+      <p>{giftCard.instructions}</p>
+      {
+        giftCard.valid && <button onClick={spendGiftCard}>
+          Spend Gift Card
+        </button>
+      }
+    </div>
+  )
 }
 
 export default App
