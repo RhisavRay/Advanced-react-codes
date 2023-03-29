5ae4ebd0327510bf27c0295934ef6ce2f8888891
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-29 05:53:02 +0530

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
+import { useRef } from "react";
 import "./App.css";
-import { useReducer } from "react";
+
 
 function App()
 {
-  const reduce = (wallet, action) =>
-  {
-    if(action.type === "buy") return {money: wallet.money - 10}
-    if(action.type === "sell") return {money: wallet.money + 10}
-    if(action.type === "celebrity") return {money: wallet.money + 5000}
-  }
+  const formInputRef = useRef(null)
 
-  const [wallet, setWallet] = useReducer(reduce, {money: 100})
+  const focusInput = () =>
+  {
+    formInputRef.current.focus()
+  }
 
   return(
     <div>
-      <h1>Wallet: {wallet.money}</h1>
-      <div>
-        <button onClick={() => setWallet({type: "buy"})}>
-          Buy ingredients
-        </button>
-        <button onClick={() => setWallet({type: "sell"})}>
-          Sell a meal
-        </button>
-        <button onClick={() => setWallet({type: "celebrity"})}>
-          Celebrity visit
-        </button>
-      </div>
+      <h1>Using useRef hook to access the underlying DOM</h1>
+      <input type='text' ref={formInputRef}/>
+      <button onClick={focusInput}>
+        Focus input
+      </button>
     </div>
   )
 }
