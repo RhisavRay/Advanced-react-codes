0059d10c2ada5b5876943a896260bcb8e46c5965
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-31 06:56:19 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,79 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-
-const withMousePosition = (WrappedComponent) =>
-{
-  return (props) =>
-  {
-    const [mousePosition, setMousePosition] = useState({
-      x: 0,
-      y: 0
-  })
-
-    useEffect(() =>
-    {
-      const handleMousePositionChange = (e) =>
-      {
-        setMousePosition({
-          x: e.clientX,
-          y: e.clientY
-        })
-      }
-      
-      window.addEventListener("mousemove", handleMousePositionChange)
-
-      return () =>
-      {
-        window.removeEventListener("mousemove", handleMousePositionChange)
-      }
-    }, [])
-    
-    return <WrappedComponent {...props} mousePosition={mousePosition}/>
-  }
-}
-
-const PannelMouseLogger = ({ mousePosition }) =>
-{
-  if(!mousePosition)
-  {
-    return null
-  }
-  
-  return(
-    <div>
-      <p>Mouse position: </p>
-      <div>
-        <span>x: {mousePosition.x}</span>
-        <span>y: {mousePosition.y}</span>
-      </div>
-    </div>
-  )
-}
-
-const PointMouseLogger = ({ mousePosition }) =>
-{
-  if(!mousePosition)
-  {
-    return null
-  }
-  
-  return(
-    <p>({mousePosition.x}, {mousePosition.y})</p>
-  )
-}
-
-const PannelMouseTracker = withMousePosition(PannelMouseLogger)
-const PointMouseTracker = withMousePosition(PointMouseLogger)
 
 function App()
 {
-  return(
-    <div>
-      <header>Little Lemon Restaurant</header>
-      <PannelMouseTracker/>
-      <PointMouseTracker/>
-    </div>
-  )
+  return
 }
  
 export default App
